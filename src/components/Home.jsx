7429f7cb9e8a9d6d59e4bc45b552a0cc5f0db080
유지수 <jsyu333@gmail.com>
2025-06-21 21:49:33 +0900

    chore: hover animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,16 +4,22 @@ function Home() {
   return (
     <motion.div
       className="max-w-2xl mx-auto pt-10"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      initial={{ x: "-100vw" }}
+      animate={{ x: 0 }}
+      transition={{ delay: 0.4, type: "spring", stiffness: 120 }}
     >
-      <motion.button
-        className="px-2 py-2 text-white bg-black rounded-md"
-        initial={{ x: "-100vw" }}
-        animate={{ x: 0 }}
-        transition={{ delay: 0.4, type: "spring", stiffness: 120 }}
-      >
+      <motion.button className="px-2 py-2 text-white bg-black rounded-md">
         Let's go!
+      </motion.button>
+      <motion.button
+        className="px-2 py-2 text-white bg-slate-500 rounded-md"
+        whileHover={{
+          scale: 1.1,
+          textShadow: "0px 0px 8px rgba(0,0,0,.5)",
+          boxShadow: "0px 0px 8px rgba(0,0,0,.5)",
+        }}
+      >
+        hover button!
       </motion.button>
     </motion.div>
   );
