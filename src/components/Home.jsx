dab83a87f3efcc20508ebd7450f9f9cf54ab7127
유지수 <jsyu333@gmail.com>
2025-06-24 00:57:16 +0900

    chore: Variants (part 1)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,28 @@
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "-100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      delay: 0.4,
+      type: "spring",
+      stiffness: 120,
+    },
+  },
+};
+
 function Home() {
   return (
     <motion.div
       className="max-w-2xl mx-auto pt-10"
-      initial={{ x: "-100vw" }}
-      animate={{ x: 0 }}
-      transition={{ delay: 0.4, type: "spring", stiffness: 120 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <motion.button className="px-2 py-2 text-white bg-black rounded-md">
         Let's go!
